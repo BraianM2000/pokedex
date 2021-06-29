6ca3274e07ff4af2a49bb4ed435ec70e73c86c6f
braian morales <braian.morales@correounivalle.edu.co>
2021-06-28 20:29:28 -0500

    se añadieron los nombres de los tipos en la lista principal

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -31,30 +31,31 @@ export const PokemonList = () => {
                                 id: data.id,
                                 name: data.name,
                                 sprite: data.sprites.front_default,
-
+                                types: data.types.map(type => {
+                                    return type.type.name
+                                })
                             }
-                            return setPokemonList(poke => [...poke, pokeData])
-                        })
+                            
+                             return pokeData
+                        }).then(pokeData=>setPokemonList(poke => [...poke, pokeData]))
 
                 })
             ))
-               
-            
-
     }
 
     pokemonList.sort(function (a, b) {
         if (a.id > b.id) {
-          return 1;
+            return 1;
         }
         if (a.id < b.id) {
-          return -1;
+            return -1;
         }
         // a must be equal to b
         return 0;
-      })
+    })
 
-    console.log(pokemonList)
+console.log(pokemonList)
+
 
     return (
         <div className="div">
@@ -64,20 +65,30 @@ export const PokemonList = () => {
                     <th>id</th>
                     <th>sprite</th>
                     <th>nombre</th>
+                    <th>tipo 1</th>
+                        <th>tipo 2</th>
+                    
+
                 </tr>
                 {
                     pokemonList.map(pokeName => {
                         return <tr>
-                             <td className="id">{pokeName.id}</td>
-                              <td><img src={pokeName.sprite} alt={pokeName.name} /></td>
-                                <td><small key={pokeName.name}> {pokeName.name}</small></td>
-                              
-                            </tr>
-                        
+                            <td className="id">{pokeName.id}</td>
+                            <td><img src={pokeName.sprite} alt={pokeName.name} /></td>
+
+                            <td><small key={pokeName.name}> {pokeName.name}</small></td>
+                            
+                            {
+                               pokeName.types.length<2 ? (<><td>{pokeName.types}</td><td></td></>): pokeName.types.map(type=>{return <td> {type} </td> }) 
+                            }
+                           
+                        </tr>
+
                     }
 
                     )
                 }
+
 
             </table>
 
